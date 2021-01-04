1b71e49a04ef672ee75ed6870503a419a9d15107
sinha-shaurya <53684163+sinha-shaurya@users.noreply.github.com>
2021-01-04 22:17:36 +0530

    Obfuscated C code for Hello World

---
+++ hello.c
@@ -0,0 +1,13 @@
+#define u unsigned char
+#define v while(*x)
+#define z(x) putchar(*x);
+#define y(x) ++*x;
+#define p(x) ++x;
+#define e(x) --*x;
+#define d(x) --x;
+#define w(x) x x
+main(){u *x=calloc(12,1);u *l=x;w(w(w(y(x))))w(y(x))v{p(x)w(w(y(x)))w(y(x))y(x)p
+(x)w(w(w(y(x))))w(y(x))p(x)w(y(x))y(x)p(x)w(w(w(y(x))))y(x)w(w(d(x)))e(x)}p(x)w(
+y(x))z(x)p(x)y(x)z(x)w(w(y(x)))w(y(x))y(x)w(z(x))w(y(x))y(x)z(x)p(x)w(y(x))z(x)p
+(x)w(e(x))e(x)z(x)w(d(x))z(x)w(y(x))y(x)z(x)w(w(e(x)))w(e(x))z(x)w(w(w(e(x))))z(
+x)p(x)y(x)z(x)free(l);}
